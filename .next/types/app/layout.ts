2f5778404c490c24669dd3ef3a83da05af8c3671
Saad Khalid <saadkhalid@192.168.0.103>
2026-07-02 03:51:41 +0500

    Website with SEO + Upgrades

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\LiteBook\Downloads\Website_Client_2\app\layout.tsx
+// File: /Users/saadkhalid/Downloads/Adel_Cooling_Website/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\LiteBook\Downloads\Website_Client_2\app\page.tsx
+// File: /Users/saadkhalid/Downloads/Adel_Cooling_Website/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
